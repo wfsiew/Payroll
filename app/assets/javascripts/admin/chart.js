58b3ca28eda9969a5c86bc303a3308288a8a2389
wfsiew <siewwingfei@hotmail.com>
2013-01-09 22:32:14 +0800

    commit

--- app/assets/javascripts/admin/chart.js
+++ app/assets/javascripts/admin/chart.js
@@ -1,149 +1,29 @@
 var cht = ( function() {
 
+    // Radialize the colors
+    Highcharts.getOptions().colors = $.map(Highcharts.getOptions().colors, function(color) {
+      return {
+        radialGradient : {
+          cx : 0.5,
+          cy : 0.3,
+          r : 0.7
+        },
+        stops : [[0, color], [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+        ]
+      };
+    });
+
     function init() {
-      chart();
       chart1();
-    }
-
-    function chart() {
-      var colors = Highcharts.getOptions().colors, 
-      categories = ['MSIE', 'Firefox', 'Chrome', 'Safari', 'Opera'], 
-      name = 'Browser brands', 
-      data = [{
-        y : 55.11,
-        color : colors[0],
-        drilldown : {
-          name : 'MSIE versions',
-          categories : ['MSIE 6.0', 'MSIE 7.0', 'MSIE 8.0', 'MSIE 9.0'],
-          data : [10.85, 7.35, 33.06, 2.81],
-          color : colors[0]
-        }
-      }, {
-        y : 21.63,
-        color : colors[1],
-        drilldown : {
-          name : 'Firefox versions',
-          categories : ['Firefox 2.0', 'Firefox 3.0', 'Firefox 3.5', 'Firefox 3.6', 'Firefox 4.0'],
-          data : [0.20, 0.83, 1.58, 13.12, 5.43],
-          color : colors[1]
-        }
-      }, {
-        y : 11.94,
-        color : colors[2],
-        drilldown : {
-          name : 'Chrome versions',
-          categories : ['Chrome 5.0', 'Chrome 6.0', 'Chrome 7.0', 'Chrome 8.0', 'Chrome 9.0', 'Chrome 10.0', 'Chrome 11.0', 'Chrome 12.0'],
-          data : [0.12, 0.19, 0.12, 0.36, 0.32, 9.91, 0.50, 0.22],
-          color : colors[2]
-        }
-      }, {
-        y : 7.15,
-        color : colors[3],
-        drilldown : {
-          name : 'Safari versions',
-          categories : ['Safari 5.0', 'Safari 4.0', 'Safari Win 5.0', 'Safari 4.1', 'Safari/Maxthon', 'Safari 3.1', 'Safari 4.1'],
-          data : [4.55, 1.42, 0.23, 0.21, 0.20, 0.19, 0.14],
-          color : colors[3]
-        }
-      }, {
-        y : 2.14,
-        color : colors[4],
-        drilldown : {
-          name : 'Opera versions',
-          categories : ['Opera 9.x', 'Opera 10.x', 'Opera 11.x'],
-          data : [0.12, 0.37, 1.65],
-          color : colors[4]
-        }
-      }];
-
-      // Build the data arrays
-      var browserData = [];
-      var versionsData = [];
-      for (var i = 0; i < data.length; i++) {
-
-        // add browser data
-        browserData.push({
-          name : categories[i],
-          y : data[i].y,
-          color : data[i].color
-        });
-
-        // add version data
-        for (var j = 0; j < data[i].drilldown.data.length; j++) {
-          var brightness = 0.2 - (j / data[i].drilldown.data.length) / 5;
-          versionsData.push({
-            name : data[i].drilldown.categories[j],
-            y : data[i].drilldown.data[j],
-            color : Highcharts.Color(data[i].color).brighten(brightness).get()
-          });
-        }
-      }
-
-      // Create the chart
-      chart = new Highcharts.Chart({
-        chart : {
-          renderTo : 'cht',
-          type : 'pie'
-        },
-        title : {
-          text : 'Browser market share, April, 2011'
-        },
-        yAxis : {
-          title : {
-            text : 'Total percent market share'
-          }
-        },
-        plotOptions : {
-          pie : {
-            shadow : false
-          }
-        },
-        tooltip : {
-          valueSuffix : '%'
-        },
-        series : [{
-          name : 'Browsers',
-          data : browserData,
-          size : '60%',
-          dataLabels : {
-            formatter : function() {
-              return this.y > 5 ? this.point.name : null;
-            },
-            color : 'white',
-            distance : -30
-          }
-        }, {
-          name : 'Versions',
-          data : versionsData,
-          innerSize : '60%',
-          dataLabels : {
-            formatter : function() {
-              // display only if larger than 1
-              return this.y > 1 ? '<b>' + this.point.name + ':</b> ' + this.y + '%' : null;
-            }
-          }
-        }]
-      });
+      chart2();
     }
 
     function chart1() {
-      // Radialize the colors
-      Highcharts.getOptions().colors = $.map(Highcharts.getOptions().colors, function(color) {
-        return {
-          radialGradient : {
-            cx : 0.5,
-            cy : 0.3,
-            r : 0.7
-          },
-          stops : [[0, color], [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
-          ]
-        };
-      });
 
       // Build the chart
-      chart = new Highcharts.Chart({
+      var chart = new Highcharts.Chart({
         chart : {
-          renderTo : 'cht1',
+          renderTo : 'cht',
           plotBackgroundColor : null,
           plotBorderWidth : null,
           plotShadow : false
@@ -171,13 +51,75 @@ var cht = ( function() {
         },
         series : [{
           type : 'pie',
-          name : 'Browser share',
-          data : [['Firefox', 45.0], ['IE', 26.8], {
-            name : 'Chrome',
+          name : 'Payroll for 2013',
+          data : [['January', 10], ['February', 10], ['March', 10], ['April', 40], ['May', 5], ['June', 5], ['July', 5], ['August', 5], {
+            name : 'September',
             y : 12.8,
             sliced : true,
             selected : true
-          }, ['Safari', 8.5], ['Opera', 6.2], ['Others', 0.7]]
+          }, ['October', 5], ['November', 3], ['December', 2]]
+        }]
+      });
+    }
+
+    function chart2() {
+      var chart = new Highcharts.Chart({
+        chart : {
+          renderTo : 'cht1',
+          type : 'column'
+        },
+        title : {
+          text : 'Monthly Average Rainfall'
+        },
+        subtitle : {
+          text : 'Source: WorldClimate.com'
+        },
+        xAxis : {
+          categories : ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis : {
+          min : 0,
+          title : {
+            text : 'Rainfall (mm)'
+          }
+        },
+        legend : {
+          layout : 'vertical',
+          backgroundColor : '#FFFFFF',
+          align : 'left',
+          verticalAlign : 'top',
+          x : 100,
+          y : 70,
+          floating : true,
+          shadow : true
+        },
+        tooltip : {
+          formatter : function() {
+            return '' + this.x + ': ' + this.y + ' mm';
+          }
+        },
+        plotOptions : {
+          column : {
+            pointPadding : 0.2,
+            borderWidth : 0
+          }
+        },
+        series : [{
+          name : 'Tokyo',
+          data : [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+
+        }, {
+          name : 'New York',
+          data : [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+
+        }, {
+          name : 'London',
+          data : [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
+
+        }, {
+          name : 'Berlin',
+          data : [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
+
         }]
       });
     }
@@ -189,4 +131,4 @@ var cht = ( function() {
     return {
       load : load
     };
-  }());
+  }()); 
