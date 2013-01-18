38fc916092abab89c9934f4cca59c379c5910c3e
wfsiew <siewwingfei@hotmail.com>
2013-01-18 22:56:54 +0800

    commit

--- config/application.rb
+++ config/application.rb
@@ -59,10 +59,10 @@ module Payroll
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
-    config.assets.precompile += %w(loginui.css _payslip.css _payslip.js blitzer/jquery-ui-1.9.2.custom.min.css admin.css admin.js user.css user.js)
+    config.assets.precompile += %w(loginui.css _payslip.css _payslip.js blitzer/jquery-ui-1.10.0.custom.min.css admin.css admin.js user.css user.js)
     
     jquitheme = %w(blitzer dark-hive trontastic humanity)
-    jquicss = 'jquery-ui-1.9.2.custom.min.css'
+    jquicss = 'jquery-ui-1.10.0.custom.min.css'
     
     jquitheme.each do |t|
       config.assets.precompile << "#{t}/#{jquicss}"
